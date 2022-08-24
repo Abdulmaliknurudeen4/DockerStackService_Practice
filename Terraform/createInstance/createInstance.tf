2c2994687da841f3dec4c59323a026c0efa4b217
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-25 00:01:18 +0100

    creating Credentials in a Seperate Centralized File

--- Terraform/createInstance/createInstance.tf
+++ Terraform/createInstance/createInstance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIA43OIJPT5MM3C532S"
-  secret_key = "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"
-  region = "us-east-2"
-}
-
 resource "aws_instance" "MyFirstInstance" {
   ami = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
