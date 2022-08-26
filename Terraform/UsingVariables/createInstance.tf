17095b76b85bc3e30760527bb238cfcac5c6a12d
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-26 18:38:49 +0100

    Using List type for security Groups, and Using Map to Looking Up AWS Region AMI values. Doesn't Include Complex Object Types.

--- Terraform/UsingVariables/createInstance.tf
+++ Terraform/UsingVariables/createInstance.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "MyFirstInstance" {
-  ami = "ami-05803413c51f242b7"
+  ami = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   count = 3
 
   tags={
     Name = "demoinstance-${count.index}"
   }
+
+  security_groups = "${var.Security_Group}"
 }
--- Terraform/UsingVariables/variable.tf
+++ Terraform/UsingVariables/variable.tf
@@ -13,3 +13,18 @@ variable "AWS_REGION" {
   default = "us-east-2"
 
 }
+
+variable "Security_Group" {
+  type = list
+  default = ["sg-242334", "sg-2329832", "sg-23432892"]
+}
+
+variable "AMIS" {
+  type = map
+  default = {
+    us-east-1 = "ami-0c77c2e3d6602e7a5"
+    us-east-2 = "ami-05803413c51f242b7"
+    us-west-1 = "ami-007ef5e8a5e43d323"
+    us-west-2 = "ami-07b8a117da8f2c473"
+  }
+}
