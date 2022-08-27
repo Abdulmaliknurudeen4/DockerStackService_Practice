b057cb5d5664755131b94dee5e4bcdb0003d738c
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-27 10:31:44 +0100

    Security Group DataSource Work

---
+++ Terraform/DataSource in Terraform/securitygroup.tf
@@ -0,0 +1,19 @@
+data "aws_ip_ranges" "us_east_ip_range" {
+  regions = ["us-east-1", "us-east-2"]
+  services = [ "ec2" ]
+}
+resource "aws_security_group" "sg-custom_us_east1" {
+  ingress = [ {
+    cidr_blocks = data.aws_ip_ranges.us_east_ip_range.cidr_blocks
+    description = "value"
+    from_port = "443"
+    protocol = "TCP"
+    self = false
+    to_port = 443
+  } ]
+
+  tags = {
+    CreateDate = data.aws_ip_ranges.us_east_ip_range.create_data
+    SyncToken = data.aws_ip_ranges.us_east_ip_range.sync_token
+  }
+}
