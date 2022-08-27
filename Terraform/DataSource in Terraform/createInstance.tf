38fa61042f619a4f814779090f1cdf765d4f833f
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-27 09:39:21 +0100

    availability zones

--- Terraform/DataSource in Terraform/createInstance.tf
+++ Terraform/DataSource in Terraform/createInstance.tf
@@ -5,7 +5,7 @@ data "aws_availability_zones" "availability" {
 resource "aws_instance" "MyFirstInstance" {
   ami = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zones.availability.names[1]
+  availability_zone = data.aws_availability_zones.availability.names[0]
 
   tags = {
     "Name" = "custom_instance"
