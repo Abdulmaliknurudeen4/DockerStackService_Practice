88ddb598966a0e690f1bdef4c373ee816bf2c7ab
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-31 07:04:53 +0100

    adding ip test

--- Terraform/Terraform for AWS Cloud/VPC_NAP_EC2_Instace/security_group.tf
+++ Terraform/Terraform for AWS Cloud/VPC_NAP_EC2_Instace/security_group.tf
@@ -12,8 +12,7 @@ resource "aws_security_group" "allow-levelup-ssh" {
     to_port          = 22
     protocol         = "tcp"
     #specify your machine's IP address for access purpose
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = ["161.35.96.155"]
   }
 
   egress {
