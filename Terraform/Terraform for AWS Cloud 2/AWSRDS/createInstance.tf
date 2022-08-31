4f0d36087f5a955a7ae723002d3fdb5bbdcb7070
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-31 17:25:18 +0100

    security group

--- Terraform/Terraform for AWS Cloud 2/AWSRDS/createInstance.tf
+++ Terraform/Terraform for AWS Cloud 2/AWSRDS/createInstance.tf
@@ -1,21 +1,22 @@
 resource "aws_key_pair" "levelup_key" {
-  key_name = "levelup_key"
-  public_key = file(var.PATH_TO_PUBLIC_KEY)
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-resource "aws_instance" "MyFirstInstance" {
-  ami = lookup(var.AMIS, var.AWS_REGION)
-  availability_zone = "us-east-2a"
+#Create AWS Instance
+resource "aws_instance" "MyFirstInstnace" {
+  ami  = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name = aws_key_pair.levelup_key.key_name
+  availability_zone = "us-east-2a"
+  key_name      = aws_key_pair.levelup_key.key_name
   vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
   subnet_id = aws_subnet.levelupvpc-public-1.id
 
   tags = {
-    "Name" = "custom_instance"
+    Name = "custom_instance"
   }
 }
 
 output "public_ip" {
-  value = aws_instance.MyFirstInstance.public_ip
+  value = aws_instance.MyFirstInstnace.public_ip 
 }
