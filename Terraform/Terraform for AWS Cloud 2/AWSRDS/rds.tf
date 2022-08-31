61372121c37267a2f565e944449a47c6608c93d2
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-31 14:40:32 +0100

    Create RDS Instance of MariaDB with Terraform

---
+++ Terraform/Terraform for AWS Cloud 2/AWSRDS/rds.tf
@@ -0,0 +1,43 @@
+#RDS Resources
+resource "aws_db_subnet_group" "mariadb-subnets" {
+  name = "mariadb-subnets"
+  description = "Amazon RDS subnet grouP"
+  subnet_ids = [aws_subnet.levelupvpc-private-1.id, aws_subnet.levelupvpc-private-2.id]
+}
+
+resource "aws_db_parameter_group" "levelup-mariadb-parameters" {
+  name = "levelup-mariadb-parameters"
+  family = "mariadb10.4"
+  description = "Maria DB parameter Group"
+
+  parameter {
+    name = "max_allowed_packets"
+    value="16777216"
+  }
+}
+# Define the RDS INstance Properties
+resource "aws_db_instance" "levelup-mariadb" {
+  allocated_storage = 20
+  engine = "mariadb"
+  engine_version = "10.4.0"
+  instance_class = "db.t2.micro"
+  identifier = "mariadb"
+  name = "mariadb"
+  username = "root"
+  password = "mariadb141"
+  db_subnet_group_name = aws_db_subnet_group.mariadb-subnets.name
+  parameter_group_name = aws_db_parameter_group.levelup-mariadb-parameters.name
+  multi_az = false
+  vpc_security_group_ids = [aws_security_group.allow-mariadb.id]
+  storage_type = "gp2"
+  backup_retention_period = 30 # how long to keep your backup
+  availability_zone = aws_subnet.levelupvpc-private-1.availability_zone # preferred AZ
+  skip_final_snapshot = true
+  tags = {
+    "Name" = "levelup-mariadb"
+  }
+}
+
+output "rds" {
+  value = aws_db_instance.levelup-mariadb.endpoint
+}
