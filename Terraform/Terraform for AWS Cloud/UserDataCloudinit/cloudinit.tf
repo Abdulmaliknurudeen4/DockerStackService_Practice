afe38396c39d590b0e83141f23571d4a40ecdcd4
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-08-31 10:31:20 +0100

    cloud init mistake

--- Terraform/Terraform for AWS Cloud/UserDataCloudinit/cloudinit.tf
+++ Terraform/Terraform for AWS Cloud/UserDataCloudinit/cloudinit.tf
@@ -4,7 +4,7 @@ data "template_file" "install_apache" {
 
 data "template_cloudinit_config" "install_apache_config" {
   gzip = false
-  base64 = false
+  base64base64_encode  = false
 
   part {
     filename = "init.cfg"
