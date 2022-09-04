6f54e23bb850c2208f9bf96ab5ee8ed5df1e3777
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-09-04 11:41:44 +0100

    changes

--- Terraform/NetworkModule/main.tf
+++ Terraform/NetworkModule/main.tf
@@ -1,6 +1,8 @@
 #Provider
 provider "aws" {
-  region = var.region
+  access_key = "AKIA43OIJPT5PTZFKCDR"
+  secret_key = "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"
+  region = "us-east-2"
 }
 
 #module
--- Terraform/NetworkModule/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  access_key = "AKIA43OIJPT5PTZFKCDR"
-  secret_key = "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"
-  region = "us-east-2"
-}
