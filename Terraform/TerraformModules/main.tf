f7bc205d2f6dff2f4b3028642b6576bf0214af7e
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-09-04 10:52:41 +0100

    Using GitHub sourced modules

---
+++ Terraform/TerraformModules/main.tf
@@ -0,0 +1,15 @@
+module "ec2_cluster" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+  name = "myFirstInstance"
+
+  ami                    = "ami-05803413c51f242b7"
+  instance_type          = "t2.micro"
+  key_name               = "levelup_key"
+  monitoring             = true
+  subnet_id              = "subnet-0480ba67e9338a3e7"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
---
+++ Terraform/TerraformModules/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIA43OIJPT5MM3C532S"
+  secret_key = "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"
+  region = "us-east-2"
+}
