3d55434a57aaccab71cec3111a0905ba053ce36f
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-09-04 10:56:01 +0100

    removed keys

--- Terraform/TerraformModules/main.tf
+++ Terraform/TerraformModules/main.tf
@@ -4,7 +4,6 @@ module "ec2_cluster" {
 
   ami                    = "ami-05803413c51f242b7"
   instance_type          = "t2.micro"
-  key_name               = "levelup_key"
   monitoring             = true
   subnet_id              = "subnet-0480ba67e9338a3e7"
 
