0217b8c6a562aca05c2f12e1d32ef39de9b3ff0c
Abdulmaliknurudeen4 <Abdulmaliknurudeen4@gmail.com>
2022-09-06 08:04:08 +0100

    little corrections

--- Terraform/caseStudy#1/main.tf
+++ Terraform/caseStudy#1/main.tf
@@ -1,23 +1,22 @@
-# module "levelup-vpc" {
-#     source      = "./module/vpc"
+module "levelup-vpc" {
+    source      = "./module/vpc"
 
-#     ENVIRONMENT = var.ENVIRONMENT
-#     AWS_REGION  = var.AWS_REGION
-# }
+    ENVIRONMENT = var.ENVIRONMENT
+    AWS_REGION  = var.AWS_REGION
+}
 
 module "levelup-webserver" {
     source      = "./webserver"
 
     ENVIRONMENT = var.ENVIRONMENT
     AWS_REGION  = var.AWS_REGION
-    # vpc_private_subnet1 = module.levelup-vpc.private_subnet1_id
-    # vpc_private_subnet2 = module.levelup-vpc.private_subnet2_id
-    # vpc_id = module.levelup-vpc.my_vpc_id
-    # vpc_public_subnet1 = module.levelup-vpc.public_subnet1_id
-    # vpc_public_subnet2 = module.levelup-vpc.public_subnet2_id
+    vpc_private_subnet1 = module.levelup-vpc.private_subnet1_id
+    vpc_private_subnet2 = module.levelup-vpc.private_subnet2_id
+    vpc_id = module.levelup-vpc.my_vpc_id
+    vpc_public_subnet1 = module.levelup-vpc.public_subnet1_id
+    vpc_public_subnet2 = module.levelup-vpc.public_subnet2_id
 
 }
-
 #Define Provider
 provider "aws" {
    access_key = "AKIA43OIJPT5PTZFKCDR"
